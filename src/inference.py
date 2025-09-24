eb42d5668603d8c453ddadc7f3cc937cd0c99f4a
IysI-99999955 <changwing@naver.com>
2025-09-24 07:09:03 +0000

    fixed

--- src/inference.py
+++ src/inference.py
@@ -44,7 +44,7 @@ def infer_and_eval(args):
     # test_dataset = load_data(args.dataset_name, split="test")
     # HuggingFace 사용으로 prepare_dataset의 args.dataset_dir -> args.dataset_name
     _,_, hate_test_dataset, test_dataset = (
-        prepare_dataset(args.dataset_name, tokenizer, args.max_len, args.model_name)
+        prepare_dataset(args.dataset_name, tokenizer, args.max_len, args.model_name, revision=args.dataset_revision)
     )
 
     # predict answer
